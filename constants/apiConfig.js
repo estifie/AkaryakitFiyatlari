0beda66a6ca46388857a44138ba2551080361c9d
LntAxe <ertusari0@gmail.com>
2024-02-29 02:04:38 +0300

    Add new endpoints for api connection

--- constants/apiConfig.js
+++ constants/apiConfig.js
@@ -1,4 +1,8 @@
 export const API_VERSION = "v1";
-export const BASE_URL = "http://192.168.1.46:3000";
-export const FUELS_URL = `${BASE_URL}/api/${API_VERSION}/fuels`;
-export const CITIES_URL = `${FUELS_URL}/cities`;
+export const BASE_URL = `http://192.168.1.46:3000`;
+export const API_URL = `${BASE_URL}/api/${API_VERSION}`;
+export const FUELS_URL = `${API_URL}/fuel`;
+export const CITIES_URL = `${API_URL}/city`;
+export const DISTRICTS_URL = `${CITIES_URL}/{cityId}/districts`;
+export const STATIONS_URL = `${API_URL}/station/stations`;
+export const PUBLIC_URL = `${BASE_URL}/public/`;
