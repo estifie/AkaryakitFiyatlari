21cea0653fd6ea35bf6cd3cdf05861c39208fefa
LntAxe <ertusari0@gmail.com>
2024-02-29 02:05:55 +0300

    Added new api connections for provider

--- context/FuelContext.js
+++ context/FuelContext.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { createContext, useContext } from "react";
-import { CITIES_URL, FUELS_URL } from "../constants/apiConfig";
+import { CITIES_URL, DISTRICTS_URL, FUELS_URL, STATIONS_URL } from "../constants/apiConfig";
 
 export const useFuel = () => {
 	const context = useContext(FuelContext);
@@ -13,9 +13,28 @@ export const useFuel = () => {
 export const FuelContext = createContext();
 
 export const FuelProvider = ({ children }) => {
+	const getStations = async () => {
+		try {
+			return axios
+				.get(`${STATIONS_URL}`)
+				.then(async (response) => {
+					const { data } = response.data;
+
+					return data;
+				})
+				.catch((error) => {
+					console.error("An error occurred", error);
+					return null;
+				});
+		} catch (error) {
+			console.error("An error occurred", error);
+			return null;
+		}
+	};
+
 	const getFuelOfCity = async (cityId) => {
 		try {
-			return axios.get(`${CITIES_URL}/${cityId}`).then(async (response) => {
+			return axios.get(`${FUELS_URL}/cities/${cityId}`).then(async (response) => {
 				const { data } = response.data;
 				if (data.status === "error") {
 					throw new Error(data.message || "An error occurred");
@@ -28,5 +47,45 @@ export const FuelProvider = ({ children }) => {
 		}
 	};
 
-	return <FuelContext.Provider value={{ getFuelOfCity }}>{children}</FuelContext.Provider>;
+	const getFuelOfCityAndDistrict = async (cityId, districtName) => {
+		try {
+			return axios
+				.get(`${DISTRICTS_URL}`.replace("{cityId}", cityId).replace("{districtName}", districtName))
+				.then(async (response) => {
+					const { data } = response.data;
+					if (data.status === "error") {
+						throw new Error(data.message || "An error occurred");
+					}
+					return data;
+				});
+		} catch (error) {
+			console.error("An error occurred", error);
+			return null;
+		}
+	};
+
+	const getDistrictsOfCity = async (cityId) => {
+		try {
+			return axios
+				.get(`${DISTRICTS_URL}`.replace("{cityId}", cityId))
+				.then(async (response) => {
+					const { data } = response.data;
+					if (data.status === "error") {
+						throw new Error(data.message || "An error occurred");
+					}
+					return data;
+				})
+				.catch((error) => {
+					console.error("An error occurred", error);
+					return null;
+				});
+		} catch (error) {
+			console.error("An error occurred", error);
+			return null;
+		}
+	};
+
+	return (
+		<FuelContext.Provider value={{ getFuelOfCity, getDistrictsOfCity, getStations }}>{children}</FuelContext.Provider>
+	);
 };
